2730555327a92c70c7632cf5c89a7409a94f1e3c
VoxlRampnt <gregoryjbull@gmail.com>
2019-10-26 20:08:26 +0200

    Generator would seldom spawn all units
    Generator would seldom spawn all units, working on a fix, code works, unity problem. now they spawn the same object without purpose.

--- New Unity Project (1)/Assets/Scripts/MapGenerator.cs
+++ New Unity Project (1)/Assets/Scripts/MapGenerator.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
     public GameObject[] objects;
-  
-    
+
+
     // Start is called before the first frame update
     void Start()
     {
